7919ce389c9cd91d4102324997f4214ea439f49f
MarekLandzwojczak <xsior18@gmail.com>
2020-06-14 17:42:38 +0200

    set read and write in club and user class

--- src/Entity/Club.php
+++ src/Entity/Club.php
@@ -2,12 +2,25 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ClubRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ *   @ApiResource( *     collectionOperations={"get","post"},
+ *     itemOperations={"get","put","delete","patch"},
+ *     normalizationContext={"groups"={"Club:read"}},
+ *     denormalizationContext={"groups"={"Club:read"}},
+ *     shortName="Club",
+ *     attributes={"pagination_items_per_page"=10,
+ *     "formats"={"jsonld","json","html"}
+ *     }
+
+ * )
+ * @ApiFilter(SearchFilter::class,properties={"name":"partial", "city":"partial"})
  * @ORM\Entity(repositoryClass=ClubRepository::class)
  */
 class Club
@@ -16,16 +29,20 @@ class Club
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"Club:read","Club:write"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"Club:read","Club:write"})
+     *
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"Club:read","Club:write"})
      */
     private $city;
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,12 +2,27 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ApiResource(
+ *     collectionOperations={"get","post"},
+ *     itemOperations={"get","put","delete","patch"},
+ *     normalizationContext={"groups"={"user:read"},"swagger_definition_name"="Read"},
+ *     denormalizationContext={"groups"={"user:write"},"swagger_definition_name"="Write"},
+ *     shortName="User",
+ *     attributes={"pagination_items_per_page"=10,
+ *     "formats"={"jsonld","json","html"}
+ *     }
+ * )
+ * @ApiFilter(SearchFilter::class,properties={"firstname":"partial", "lastname":"partial"})
  */
 class User implements UserInterface
 {
@@ -15,42 +30,50 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"user:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"user:read","user:write"})
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * @Groups({"user:read","user:write"})
      */
     private $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Groups({"user:write"})
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=50)
+     * @Groups({"user:read","user:write"})
      */
     private $firstname;
 
     /**
      * @ORM\Column(type="string", length=100)
+     * @Groups({"user:read","user:write"})
      */
     private $lastname;
 
     /**
      * @ORM\Column(type="boolean")
+     * @Groups({"user:read","user:write"})
      */
     private $statusAccaunt;
 
     /**
      * @ORM\Column(type="boolean")
+     * @Groups({"user:read","user:write"})
      */
     private $statusPlayer;
 
